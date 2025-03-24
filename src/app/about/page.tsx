e470a2acfb1c9550aa66d4cdabf9e93b1f22798a
ILFforever <hammymukura@gmail.com>
2025-03-24 11:31:24 +0700

    test

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,6 +7,8 @@ export default function About(){
                 <div> Variety of cars: Choose from a diverse range of cars to suit your needs, preferences, and favorite rental providers.</div>
                 <div> Easy Booking: Effortlessly select your rental duration, including the start and return dates, for a hassle-free experience.</div>
                 <div> Tiered Classification: access to the right car based on your eligibility and preferences.</div>
+                <div> AHHHHHHHHHHHHHHHHHHHHH</div>
+
                 </div>
                 <div className="absolute bottom-5 w-full text-center text-3xl font-bold">Reserve Your Ride Today Drive in Style, Drive with Prestige.</div>
             </main>
